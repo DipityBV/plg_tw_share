fe00503229ae4232c3b705d241777ea7a166ece0
Jasper van Rijbroek <jasper@jvar.nl>
2015-10-23 10:45:45 +0200

    Moved to a plugins structure.

---
+++ share.php
@@ -0,0 +1,94 @@
+<?php
+
+class PlgContentShare extends JPlugin
+{
+    private static $_loaded = false;
+
+    public function onContentBeforeDisplay($context, &$row, &$params, $page = 0)
+    {
+        // Here we need to add the stuff to our html.
+        $app = JFactory::getApplication();
+        $document = $app->getDocument();
+
+        if($app->isSite() && !self::$_loaded) {
+            $document->addStyleSheet(JUri::base() . '/media/plg_share/css/styling.css');
+
+            $document->addScript(JUri::base() . '/media/plg_share/js/social.js');
+            $document->addScriptDeclaration($this->_getScript());
+
+            self::$_loaded = true;
+        }
+    }
+
+    protected function _getScript()
+    {
+        // Here we wil concat the needed strings.
+        $script = 'window.addEventListener(\'load\', function(event) {
+            var social = new SocialSharing({
+                buttonElement: \'' . $this->params->get('button_element') . '\',
+                shareClass: \'' . $this->params->get('share_class') . '\',
+                popupClasses: \'' . $this->params->get('popup_class') . '\',
+                position: \'' . $this->params->get('highlight_location') . '\'
+            });';
+
+        if($this->params->get('enable_twitter')) {
+            $script .= '
+                social.addAdapter(\'tw-share__link--twitter\', \'http://twitter.com/share\', function(text) {
+                    if(text.length > 140) {
+                        text = text.substring(0, 140 - ((\'...\'.length) + (window.location.toString().length + 1)));
+                    }
+
+                    if(\'' . $this->params->get('twitter_username') . '\') {
+                        text = text.substring(0, text.length - (\' via @' . $this->params->get('twitter_username') . '\'.length));
+                    }
+
+                    var tweet = {
+                        url: window.location.toString(),
+                        text: text + \'...\'
+                    };
+
+                    if(\'' . $this->params->get('twitter_username') . '\') {
+                        tweet.via = \'' . $this->params->get('twitter_username') . '\';
+                    }
+
+                    return tweet;
+                });
+            ';
+        }
+
+        if($this->params->get('enable_facebook')) {
+            $script .= '
+                social.addAdapter(\'tw-share__link--facebook\', \'https://www.facebook.com/sharer/sharer.php\', function(text) {
+                    return {
+                        u: window.location.toString(),
+                        text: text
+                    };
+                });
+            ';
+        }
+
+        if($this->params->get('enable_pinterest')) {
+            $script .= '
+                social.addAdapter(\'tw-share__link--pinterest\', \'https://www.pinterest.com/pin/create/button/\', function(text) {
+                    return {
+                        url: window.location.toString(),
+                        description: text
+                    };
+                });
+            ';
+        }
+
+        if($this->params->get('enable_google_plus')) {
+            $script .= '
+                social.addAdapter(\'tw-share__link--google-plus\', \'https://plus.google.com/share\', function(text) {
+                    return {
+                        url: window.location.toString(),
+                        description: text
+                    };
+                });
+            ';
+        }
+
+        return $script .= '});';
+    }
+}
