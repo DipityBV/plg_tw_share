33e13b128cc8197efd0effb38b658ffb504c1e8f
Jasper van Rijbroek <jasper@jvar.nl>
2015-11-10 10:09:24 +0100

    Fixed the javascript

--- media/js/tw_share.js
+++ media/js/tw_share.js
@@ -14,23 +14,20 @@
 
     $.extend(Social, {
         getPopup: function() {
-            var popup = document.createElement('div');
-            popup.classList.add(defaults.baseClass);
+            var popup = $(document.createElement('div'));
+            popup.addClass(defaults.baseClass);
 
             defaults.adapters.forEach(function (item, key) {
-                var button = document.createElement('a');
-                button.classList.add(defaults.baseClass + '__link');
-                button.classList.add(defaults.baseClass + '__link--' + item.logo);
-                button.addEventListener('mousedown', this.prototype.shareText.bind(this, item));
+                var button = $(document.createElement('a'));
+                button.addClass(defaults.baseClass + '__link');
+                button.addClass(defaults.baseClass + '__link--' + item.logo);
+                button.on('mousedown', this.prototype.shareText.bind(this, item));
 
-                popup.appendChild(button);
+                popup.append(button);
             }.bind(this));
 
-            return $(popup).html();
-        }
-    });
-
-    $.extend(Social.prototype, {
+            return popup[0];
+        },
         getSelection: function() {
             var text = '';
 
@@ -43,7 +40,10 @@
             }
 
             return text;
-        },
+        }
+    });
+
+    $.extend(Social.prototype, {
         shareText: function(adapter) {
             var url = adapter.buildUrl.call(this, this.getSelection());
 
@@ -61,15 +61,70 @@
 
             window.open(adapter.url + '?' + url, '', 'width=715,height=450');
         },
+        getLocation: function() {
 
+        }
     });
 
     $.fn.select = function(config) {
-        this.each(function() {
-
-        });
-
         $.extend(defaults, config);
+
+        var coordinates = {
+            y: 0,
+            x: 0
+        };
+
+        this.each(function() {
+            console.log(this);
+
+            $(this).on('mousedown', function(event) {
+                // Remove all the popup elements from the document.
+                $('.' + defaults.baseClass).each(function(key, item) {
+                    if($(item).parent('.tw-share-mark').length === 0) {
+                        $(item).remove();
+                    }
+                });
+
+                // Get the coordinates
+                coordinates.y = event.clientY;
+                coordinates.x = event.clientX;
+            });
+
+            $(this).on('mouseup', function(event) {
+                var selection = Social.getSelection(),
+                    top = (event.clientY <= coordinates.y) ? coordinates.y : event.clientY,
+                    center = (coordinates.x + event.clientX) / 2,
+                    css = {
+                        top: 0,
+                        left: 0,
+                        extra_class: ''
+                    };
+
+                if(selection) {
+                    // get the position.
+                    switch(defaults.position) {
+                        case 'top':
+                            css.top = top;
+                            css.left = center;
+                            css.extra_class = 'bottom';
+                            break;
+                        default:
+                            css.top = event.clientY;
+                            css.left = event.clientX;
+                            break;
+                    }
+
+                    var popup = $(Social.getPopup());
+                    if(css.extra_class) {
+                        popup.addClass(defaults.baseClass + '--' + css.extra_class);
+                    }
+                    popup.css('top', css.top);
+                    popup.css('left', css.left);
+
+                    $(document.body).append(popup[0]);
+                }
+            });
+        });
     };
 
     $.fn.highlight = function(config) {
@@ -81,50 +136,3 @@
         });
     };
 })(window.jQuery, window);
-
-function SocialSharing(config) {
-    this.adapters = [];
-    this.config = config || {};
-    this.selection = {};
-    this.popup = undefined;
-
-    if(config.adapters && typeof config.adapters.push === 'function') {
-        for(var index in config.adapters) {
-            if(config.adapters.hasOwnProperty(index)) {
-                var adapter = config.adapters[index];
-
-                this.addAdapter(adapter.logo, adapter.url, adapter.buildUrl);
-            }
-        }
-    }
-}
-
-SocialSharing.prototype.getCoordinates = function(event, position) {
-    switch(position) {
-        case 'top':
-            if(event.target && event.target.tagName === 'SPAN') {
-                this.selection.left = ((event.target.offsetLeft + event.target.offsetLeft + event.target.offsetWidth) / 2);
-                this.selection.top = event.target.offsetTop;
-            } else {
-                this.selection.left = (this.selection.left + event.pageX) / 2;
-            }
-
-            return {
-                top: (this.selection.top - 60),
-                left: this.selection.left,
-                extraClass: 'bottom'
-            };
-            break;
-        case 'bottom':
-            // Do calc
-            break;
-        case 'left':
-            // Do calc
-            break;
-        case 'right':
-            // Do calc
-            break;
-        default:
-            return {left: event.pageX, top: event.pageY};
-    }
-};
